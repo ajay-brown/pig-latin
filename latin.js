af4bf7d2445c1b5d96ee0daf31d85ed5bc26a442
Audrey <audreybrown94@gmail.com>
2018-03-31 10:19:44 -1000

    logic works, working on form integration

--- latin.js
+++ latin.js
@@ -26,3 +26,29 @@ switch (unLatin.charAt(0)) {
     return pigLatinStart + pigLatinEnd;
     break;
 }
+
+function getString2() {
+  var unEnglish = document
+    .getElementById("frm2")
+    .submit()
+    .toString()
+    .toLowerCase();
+  return unEnglish;
+}
+switch (unEnglish.charAt(0)) {
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u":
+    let english = unEnglish.split("-");
+    return english[0];
+    break;
+  default:
+    let english2 = unEnglish.split("-");
+    let first = english2[0].join();
+    let second = english2[1];
+    let lengthSecond = second.length;
+    let secondStr = second.splice(1, lengthSecond).join();
+    return first + secondStr;
+}
